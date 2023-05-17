4d2c426c8e47b217109779e14a813943e9471c01
lebakassemmerl <hotschi@gmx.at>
2023-05-17 17:11:53 +0200

    helpers: add function to detect if number is power of 2

--- core/helpers.h
+++ core/helpers.h
@@ -15,19 +15,19 @@
 #define ARRAY_SIZE(x) (sizeof(x) / sizeof(x[0]))
 
 namespace hlp {
-template <typename T> requires std::unsigned_integral<T>
+template<typename T> requires std::unsigned_integral<T>
 constexpr T bit(T pos)
 {
     return (static_cast<T>(1) << pos);
 }
 
-template <typename T, typename V> requires std::unsigned_integral<T> && std::integral<V>
+template<typename T, typename V> requires std::unsigned_integral<T> && std::integral<V>
 constexpr T leftshift(V val, T pos)
 {
     return (static_cast<T>(val) << pos);
 }
 
-template <typename T> requires std::unsigned_integral<T>
+template<typename T> requires std::unsigned_integral<T>
 constexpr T mask(size_t bit_high, size_t bit_low)
 {
     T one = static_cast<T>(1U);
@@ -35,6 +35,12 @@ constexpr T mask(size_t bit_high, size_t bit_low)
     T step2 = step1 - static_cast<T>(((one << bit_low) - one));
     T step3 = step1 >> (((sizeof(T) * 8) - one) - bit_high);
     return step2 & step3;
+}
+
+template<size_t N>
+constexpr bool is_powerof2() noexcept
+{
+    return N && ((N & (N - 1)) == 0);
 }
 
 template<typename T> requires std::integral<T>
