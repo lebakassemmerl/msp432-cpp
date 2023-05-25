b27fa3bbdc1b79d74968e5159388ce2e0defd454
lebakassemmerl <hotschi@gmx.at>
2023-05-25 17:52:51 +0200

    nvic: do not enable unused/reserved interrupts

--- periph/cortexm4f/nvic/nvic.cpp
+++ periph/cortexm4f/nvic/nvic.cpp
@@ -27,8 +27,11 @@ void Nvic::clear_all_pending() noexcept
 
 void Nvic::enable_all_interrupts() noexcept
 {
-    reg().iser[0].set(~0);
-    reg().iser[1].set(~0);
+    constexpr uint32_t UNUSED_1 = hlp::bit<uint32_t>(27);       // shared Timer32 interrupt
+    constexpr uint32_t UNUSED_2 = hlp::mask<uint32_t>(31, 9);   // unused
+
+    reg().iser[0].set(~UNUSED_1);
+    reg().iser[1].set(~UNUSED_2);
 }
 
 void Nvic::disable_all_interrupts() noexcept
