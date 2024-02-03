3957addfa02cd0003f16c7f9ef3d50b1c94a090d
hotschi <hotschi@gmx.at>
2024-02-03 12:49:05 +0100

    dma: handle transmissions > 1024 words correctly + coding style fixes

--- periph/dma/dma_channel.cpp
+++ periph/dma/dma_channel.cpp
@@ -25,11 +25,10 @@ Err DmaChannel::setup(const DmaConfig& conf) noexcept
 
     this->conf = conf;
     ctrl_prim.ctrl.modify(
-        dmactrl::ctrl::src_size.value(static_cast<uint32_t>(conf.width))
-        + dmactrl::ctrl::dst_size.value(static_cast<uint32_t>(conf.width))
-        + dmactrl::ctrl::src_inc.value(static_cast<uint32_t>(conf.src_incr))
-        + dmactrl::ctrl::dst_inc.value(static_cast<uint32_t>(conf.dst_incr))
-    );
+        dmactrl::ctrl::src_size.value(static_cast<uint32_t>(conf.width)) +
+        dmactrl::ctrl::dst_size.value(static_cast<uint32_t>(conf.width)) +
+        dmactrl::ctrl::src_inc.value(static_cast<uint32_t>(conf.src_incr)) +
+        dmactrl::ctrl::dst_inc.value(static_cast<uint32_t>(conf.dst_incr)));
 
     reg().chx_srccfg[idx].set(conf.src_chan % (MAX_SRC_NR + 1));
 
@@ -39,6 +38,10 @@ Err DmaChannel::setup(const DmaConfig& conf) noexcept
 
 Err DmaChannel::transfer_mem_to_periph(const uint8_t* src, uint8_t* dst, uint32_t len) noexcept
 {
+    uint32_t bytes_to_transmit;
+    uint32_t src_end_ptr;
+    uint32_t dst_end_ptr;
+
     if (!in_use)
         return Err::NotInitialized;
 
@@ -53,16 +56,20 @@ Err DmaChannel::transfer_mem_to_periph(const uint8_t* src, uint8_t* dst, uint32_
 
     info.busy = true;
 
-    uint32_t src_end_ptr = reinterpret_cast<uint32_t>(src) + (len - 1);
-    uint32_t dst_end_ptr = reinterpret_cast<uint32_t>(dst);
+    if (len <= MAX_TRANSFERS_LEN)
+        bytes_to_transmit = len;
+    else
+        bytes_to_transmit = MAX_TRANSFERS_LEN;
+
+    src_end_ptr = reinterpret_cast<uint32_t>(src) + (bytes_to_transmit - 1);
+    dst_end_ptr = reinterpret_cast<uint32_t>(dst);
 
     mode = DmaMode::Basic;
     ctrl_prim.src_ptr.set(src_end_ptr);
     ctrl_prim.dst_ptr.set(dst_end_ptr);
     ctrl_prim.ctrl.modify(
-        dmactrl::ctrl::src_inc.value(static_cast<uint32_t>(conf.src_incr))
-        + dmactrl::ctrl::dst_inc.value(static_cast<uint32_t>(conf.dst_incr))
-    );
+        dmactrl::ctrl::src_inc.value(static_cast<uint32_t>(conf.src_incr)) +
+        dmactrl::ctrl::dst_inc.value(static_cast<uint32_t>(conf.dst_incr)));
 
     info.src = src;
     info.dst = dst;
@@ -79,6 +86,10 @@ Err DmaChannel::transfer_mem_to_periph(const uint8_t* src, uint8_t* dst, uint32_
 
 Err DmaChannel::transfer_periph_to_mem(const uint8_t* src, uint8_t* dst, uint32_t len) noexcept
 {
+    uint32_t bytes_to_transmit;
+    uint32_t src_end_ptr;
+    uint32_t dst_end_ptr;
+
     if (!in_use)
         return Err::NotInitialized;
 
@@ -93,16 +104,20 @@ Err DmaChannel::transfer_periph_to_mem(const uint8_t* src, uint8_t* dst, uint32_
 
     info.busy = true;
 
-    uint32_t src_end_ptr = reinterpret_cast<uint32_t>(src);
-    uint32_t dst_end_ptr = reinterpret_cast<uint32_t>(dst) + (len - 1);
+    if (len <= MAX_TRANSFERS_LEN)
+        bytes_to_transmit = len;
+    else
+        bytes_to_transmit = MAX_TRANSFERS_LEN;
+
+    src_end_ptr = reinterpret_cast<uint32_t>(src);
+    dst_end_ptr = reinterpret_cast<uint32_t>(dst) + (bytes_to_transmit - 1);
 
     mode = DmaMode::Basic;
     ctrl_prim.src_ptr.set(src_end_ptr);
     ctrl_prim.dst_ptr.set(dst_end_ptr);
     ctrl_prim.ctrl.modify(
-        dmactrl::ctrl::src_inc.value(static_cast<uint32_t>(conf.src_incr))
-        + dmactrl::ctrl::dst_inc.value(static_cast<uint32_t>(conf.dst_incr))
-    );
+        dmactrl::ctrl::src_inc.value(static_cast<uint32_t>(conf.src_incr)) +
+        dmactrl::ctrl::dst_inc.value(static_cast<uint32_t>(conf.dst_incr)));
 
     info.src = src;
     info.dst = dst;
@@ -111,7 +126,7 @@ Err DmaChannel::transfer_periph_to_mem(const uint8_t* src, uint8_t* dst, uint32_
     calc_remaining_words(len);
     info.type = DmaTransferType::PeripheralToMemory;
 
-    config_prim_channel(len);
+    config_prim_channel(bytes_to_transmit);
     enable_channel();
 
     return Err::Ok;
@@ -120,6 +135,10 @@ Err DmaChannel::transfer_periph_to_mem(const uint8_t* src, uint8_t* dst, uint32_
 Err DmaChannel::transfer_custom(const uint8_t* src, uint8_t* dst, DmaPtrIncrement src_incr,
     DmaPtrIncrement dst_incr, uint32_t num_bytes) noexcept
 {
+    uint32_t bytes_to_transmit;
+    uint32_t src_end_ptr;
+    uint32_t dst_end_ptr;
+
     if (!in_use)
         return Err::NotInitialized;
 
@@ -134,21 +153,26 @@ Err DmaChannel::transfer_custom(const uint8_t* src, uint8_t* dst, DmaPtrIncremen
 
     info.busy = true;
 
-    uint32_t src_end_ptr = reinterpret_cast<uint32_t>(src);
-    uint32_t dst_end_ptr = reinterpret_cast<uint32_t>(dst);
+    if (num_bytes <= MAX_TRANSFERS_LEN)
+        bytes_to_transmit = num_bytes;
+    else
+        bytes_to_transmit = MAX_TRANSFERS_LEN;
+
+    src_end_ptr = reinterpret_cast<uint32_t>(src);
+    dst_end_ptr = reinterpret_cast<uint32_t>(dst);
+
     if (src_incr != DmaPtrIncrement::NoIncr)
-        src_end_ptr += num_bytes - 1;
+        src_end_ptr += bytes_to_transmit - 1;
 
     if (dst_incr != DmaPtrIncrement::NoIncr)
-        dst_end_ptr += num_bytes - 1;
+        dst_end_ptr += bytes_to_transmit - 1;
 
     mode = DmaMode::Basic;
     ctrl_prim.src_ptr.set(src_end_ptr);
     ctrl_prim.dst_ptr.set(dst_end_ptr);
     ctrl_prim.ctrl.modify(
-        dmactrl::ctrl::src_inc.value(static_cast<uint32_t>(src_incr))
-        + dmactrl::ctrl::dst_inc.value(static_cast<uint32_t>(dst_incr))
-    );
+        dmactrl::ctrl::src_inc.value(static_cast<uint32_t>(src_incr)) +
+        dmactrl::ctrl::dst_inc.value(static_cast<uint32_t>(dst_incr)));
 
     info.src = src;
     info.dst = dst;
@@ -157,7 +181,7 @@ Err DmaChannel::transfer_custom(const uint8_t* src, uint8_t* dst, DmaPtrIncremen
     calc_remaining_words(num_bytes);
     info.type = DmaTransferType::PeripheralToMemory;
 
-    config_prim_channel(num_bytes);
+    config_prim_channel(bytes_to_transmit);
     enable_channel();
 
     return Err::Ok;
@@ -166,6 +190,7 @@ Err DmaChannel::transfer_custom(const uint8_t* src, uint8_t* dst, DmaPtrIncremen
 void DmaChannel::calc_remaining_words(size_t num_bytes) noexcept
 {
     size_t transfers = num_bytes >> static_cast<size_t>(conf.width);
+
     if (transfers > MAX_TRANSFERS_LEN)
         info.remaining_words = transfers - MAX_TRANSFERS_LEN;
     else
@@ -174,37 +199,41 @@ void DmaChannel::calc_remaining_words(size_t num_bytes) noexcept
 
 void DmaChannel::update_dma_pointers() noexcept
 {
-    uint32_t r_power = 0;
+    uint32_t words_to_transmit;
+    uint32_t r_power;
 
-    if ((info.type == DmaTransferType::PeripheralToMemory)
-        || (info.type == DmaTransferType::MemoryToMemory)) {
+    if (info.remaining_words > MAX_TRANSFERS_LEN) {
+        words_to_transmit = MAX_TRANSFERS_LEN;
+        info.remaining_words -= MAX_TRANSFERS_LEN;
+    } else {
+        words_to_transmit = info.remaining_words;
+        info.remaining_words = 0;
+    }
+
+    if ((info.type == DmaTransferType::PeripheralToMemory) ||
+       (info.type == DmaTransferType::MemoryToMemory)) {
         ctrl_prim.dst_ptr.set(
             ctrl_prim.dst_ptr.get() + (MAX_TRANSFERS_LEN << static_cast<uint32_t>(conf.width)));
     }
 
-    if ((info.type == DmaTransferType::MemoryToPeripheral)
-        || (info.type == DmaTransferType::MemoryToMemory)) {
+    if ((info.type == DmaTransferType::MemoryToPeripheral) ||
+       (info.type == DmaTransferType::MemoryToMemory)) {
         ctrl_prim.src_ptr.set(
             ctrl_prim.src_ptr.get() + (MAX_TRANSFERS_LEN << static_cast<uint32_t>(conf.width)));
     }
 
-    if (info.remaining_words > MAX_TRANSFERS_LEN)
-        info.remaining_words -= MAX_TRANSFERS_LEN;
+    if (info.type == DmaTransferType::MemoryToMemory)
+        r_power = 31 - std::countl_zero(words_to_transmit);
     else
-        info.remaining_words = 0;
-
-    if (info.type == DmaTransferType::MemoryToMemory) {
-        if (info.remaining_words > MAX_TRANSFERS_LEN)
-            r_power = 31 - std::countl_zero(MAX_TRANSFERS_LEN);
-        else
-            r_power = 31 - std::countl_zero(info.remaining_words);
-    }
+        r_power = 0;
 
     ctrl_prim.ctrl.modify(
+        dmactrl::ctrl::n_minus_1.value(words_to_transmit - 1) +
+        dmactrl::ctrl::r_power.value(r_power) +
         dmactrl::ctrl::cycle_ctrl.value(static_cast<uint32_t>(mode))
-        + dmactrl::ctrl::n_minus_1.value((info.remaining_words - 1) % MAX_TRANSFERS_LEN)
-        + dmactrl::ctrl::r_power.value(r_power)
     );
+
+    enable_channel();
 }
 
 void DmaChannel::config_prim_channel(uint32_t bytes_to_transmit) noexcept
@@ -212,9 +241,9 @@ void DmaChannel::config_prim_channel(uint32_t bytes_to_transmit) noexcept
     uint32_t transfers = bytes_to_transmit >> static_cast<uint32_t>(conf.width);
 
     ctrl_prim.ctrl.modify(
-        dmactrl::ctrl::n_minus_1.value((transfers - 1) % MAX_TRANSFERS_LEN)
-        + dmactrl::ctrl::r_power.value(0)
-        + dmactrl::ctrl::cycle_ctrl.value(static_cast<uint32_t>(mode))
+        dmactrl::ctrl::n_minus_1.value(transfers - 1) +
+        dmactrl::ctrl::r_power.value(0) +
+        dmactrl::ctrl::cycle_ctrl.value(static_cast<uint32_t>(mode))
     );
 }
 
